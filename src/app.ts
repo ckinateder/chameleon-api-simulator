333390f82455f5e204dc1edf8436bd8e7299b98a
Gareth Fultz <garethfultz@gmail.com>
2023-05-08 18:13:04 -0400

    Added barebones front-end proof of concept

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
+import InterfaceHandler from './InterfaceHandler';
 import RequestHandler from './RequestHandler';
 
-
-const port: number = 3001;
-const dirLocation: string = "C:/Projects/chameleon-api-simulator/.testconfigs/"
+const apiPort: number = 3001;
+const interfacePort: number = 3002;
+const dirLocation: string = "./.testconfigs/"
 const baseRoute: string = `/echoAPI/v1/`
 const urlPrefix: string = `https://api-nba-v1.p.rapidapi.com/`
 
-const test: RequestHandler = new RequestHandler(port, baseRoute, dirLocation, urlPrefix);
 
-test.runRequest();
+//const test: RequestHandler = new RequestHandler(apiPort, baseRoute, dirLocation, urlPrefix);
+const testInterface: InterfaceHandler = new InterfaceHandler(interfacePort, baseRoute, dirLocation, urlPrefix);
+
+testInterface.runRequest();
 
